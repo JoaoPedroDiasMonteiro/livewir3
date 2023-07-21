71c049e931c384e0180879f113e3d4ad364c02e2
= <=>
2023-07-20 23:34:18 -0300

    Add search scope for Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,4 +11,16 @@ class Post extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function scopeSearch(Builder $query, string $search): Builder
+    {
+        if (blank($search)) {
+            return $query;
+        }
+
+        $search = "%{$search}%";
+
+        return $query->where('title', 'like', $search)
+            ->orWhere('content', 'like', $search);
+    }
 }

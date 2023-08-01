4d4f51a95545511e00c273844b2cdda9468ed8b6
João Pedro Dias <joaopedro453@hotmail.com>
2023-07-31 22:15:29 -0300

    Add GameFactory with seeder

---
+++ database/factories/GameFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Enums\GameTypes;
+use App\Models\Game;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class GameFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'type' => GameTypes::CRASH,
+            'server_seed' => Game::createSeed(),
+            'client_seed' => Game::createSeed(),
+            'result' => $this->faker->randomFloat(2, 1, 9),
+            'ends_at' => now(),
+        ];
+    }
+}

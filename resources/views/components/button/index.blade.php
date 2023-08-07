7b4ce5f15c6e8de9295ead5cb722e5f0629cffd1
João Pedro Dias <joaopedro453@hotmail.com>
2023-08-06 22:26:35 -0300

    Add disabled property to base button component

--- resources/views/components/button/index.blade.php
+++ resources/views/components/button/index.blade.php
@@ -1,13 +1,16 @@
 @props([
     'label' => null,
-    'color' => 'primary'
+    'color' => 'primary',
+    'disabled' => false,
 ])
 
-<button type="button"
-        {{ $attributes->except('class') }}
+<button type="{{ $attributes->get('type') ?? 'button' }}"
+        @if($disabled) disabled @endif
+        {{ $attributes->except(['class', 'disabled']) }}
         @class([
-            'block rounded-md bg-gradient-to-r hover:bg-gradient-to-b px-3 py-2 text-center text-sm font-semibold text-white shadow-sm transition-all hover:scale-110',
-            'focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline active:scale-95',
+            'block rounded-md bg-gradient-to-r hover:bg-gradient-to-b px-3 py-2 text-center text-sm font-semibold text-white shadow-sm transition-all hover:scale-95',
+            'focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline active:scale-100',
+            'disabled:opacity-80 disabled:cursor-not-allowed disabled:scale-100',
             'from-indigo-500 via-purple-500 to-pink-500 hover:bg-indigo-500 focus-visible:outline-indigo-600' => $color === 'primary',
         ])
 >

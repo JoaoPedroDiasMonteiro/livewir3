27f4b5a35004f4fc267fe74273e04d1dc7867147
João Pedro Dias <joaopedro453@hotmail.com>
2023-08-06 23:15:07 -0300

    WIP - Added functionality to bet on a game

--- app/Livewire/Games/Crash/Index.php
+++ app/Livewire/Games/Crash/Index.php
@@ -3,17 +3,46 @@
 namespace App\Livewire\Games\Crash;
 
 use App\Enums\GameTypes;
+use App\Models\Bet;
 use App\Models\Game;
 use Illuminate\Contracts\View\View;
 use Livewire\Attributes\Computed;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class Index extends Component
 {
+    #[Rule('required|numeric|min:1|max:100000')]
+    public $amount = '0.00';
+
+    #[Rule('required|numeric|min:0.01|max:100')]
+    public $predict = '0.00';
+
+    public function bet(): void
+    {
+        if ($this->game->finished) {
+            return;
+        }
+
+        $this->validate();
+
+        Bet::create([
+            'user_id' => auth()->user()->id,
+            'game_id' => $this->game->id,
+            'predicted_result' => $this->predict,
+            'bet_amount' => $this->amount,
+        ]);
+
+        $this->reset('predict', 'amount');
+
+        /** TODO: Notification */
+    }
+
     #[Computed] // TODO: Add cache
     public function game(): Game
     {
         return Game::query()
+            ->with('bets')
             ->whereType(GameTypes::CRASH)
             ->latest('id')
             ->first();
--- resources/views/livewire/games/crash/index.blade.php
+++ resources/views/livewire/games/crash/index.blade.php
@@ -7,11 +7,24 @@
         <p>Client seed: <span class="text-gray-500">{{ $this->game->client_seed }}</span></p>
     </header>
 
-    <section class="flex rounded-lg shadow">
+    <section class="flex rounded-lg shadow overflow-hidden">
         {{-- TODO: transform in component --}}
-        <div class="w-1/3">
-            Bet input
-        </div>
+        <form wire:submit='bet' class="flex w-1/3 flex-col gap-4 p-4">
+            <label class="flex flex-col">
+                Amount $
+                <input wire:model='amount' class="rounded-lg" type="text">
+                @error('amount') <span class="text-xs text-red-500">{{ $message }}</span> @enderror
+
+            </label>
+
+            <label class="flex flex-col">
+                Bet
+                <input wire:model='predict' class="rounded-lg" type="text">
+                @error('predict') <span class="text-xs text-red-500">{{ $message }}</span> @enderror
+            </label>
+
+            <x-button :disabled="$this->game->finished" type="submit" label="Save" />
+        </form>
 
         {{-- TODO: transform in component --}}
         <div class="flex h-80 flex-1 items-center justify-center bg-blue-300">
@@ -22,14 +35,29 @@
                 </div>
             @else
                 <div>Make your bets...</div>
-
             @endif
         </div>
     </section>
 
     <livewire:games.crash.last-results wire:key="last-results-{{ $this->game->id }}" />
 
-    <section class="h-20 rounded-lg bg-slate-50 shadow">
-        Bets component
+    {{-- TODO: Bets component --}}
+    <section class="rounded-lg shadow space-y-2 overflow-hidden p-4">
+        @foreach ($this->game->bets as $bet)
+            <div @class([
+                'flex items-center rounded-lg p-2',
+                'bg-red-200' => $this->game->finished && $bet->predicted_result > $this->game->result,
+                'bg-green-200' => $this->game->finished && $bet->predicted_result <= $this->game->result,
+            ]) wire:key='game-{{ $this->game->id }}-bet-{{ $bet->id }}'>
+                <div>
+                    {{ $bet->user->name }}
+                </div>
+
+                <div class="ml-auto">
+                    <span class="bg-gray-200 p-2 rounded-lg">{{ $bet->predicted_result }} X</span>
+                    <span class="bg-gray-200 p-2 rounded-lg ml-4">$ {{ $bet->bet_amount }}</span>
+                </div>
+            </div>
+        @endforeach
     </section>
 </div>

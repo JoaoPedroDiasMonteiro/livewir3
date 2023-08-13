72e2fdc1a195e0752397950d3e9015ecce7bc867
João Pedro Dias <joaopedro453@hotmail.com>
2023-08-13 02:21:26 -0300

    WIP - Added a temporary schedule to create bets on crash games to allow testing the ProcessCrashGameBets

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,9 +4,12 @@ namespace App\Console;
 
 use App\Actions\Game\CreateCrashGame;
 use App\Enums\GameTypes;
+use App\Models\Bet;
 use App\Models\Game;
+use App\Models\User;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\App;
 
 class Kernel extends ConsoleKernel
 {
@@ -22,6 +25,50 @@ class Kernel extends ConsoleKernel
                 CreateCrashGame::execute();
             }
         })->everyMinute();
+
+        // TODO: Move to action
+        if (! App::isProduction()) {
+            $schedule->call(function () {
+                $latestCrashGame = Game::query()
+                    ->whereType(GameTypes::CRASH)
+                    ->latest('id')
+                    ->whereFinished(false)
+                    ->first();
+
+                if (! $latestCrashGame) {
+                    return;
+                }
+
+                try {
+                    $users = User::query()
+                        ->inRandomOrder()
+                        ->where('id', '!=', 1)
+                        ->take(rand(1, 3))
+                        ->get();
+
+                    $users->each(function (User $user) use ($latestCrashGame) {
+                        usleep(rand(30000, 300000));
+
+                        $predict = fake()->randomFloat(2, 1.1, 3);
+                        $amount = fake()->randomFloat(2, 1, 20);
+
+                        $user->withdrawBalance($amount);
+
+                        Bet::create([
+                            'user_id' => $user->id,
+                            'game_id' => $latestCrashGame->id,
+                            'predicted_result' => $predict,
+                            'bet_amount' => $amount,
+                        ]);
+                    });
+
+                } catch (\Throwable $th) {
+                    echo $th->getMessage();
+                }
+
+            })->everySecond();
+        }
+
     }
 
     protected function commands(): void
